5b23493d18453c3e2a39ac32fc99fd3e15f336e4
Tor Holm <tor.holm@live.dk>
2025-11-14 12:26:56 +0100

    mango

--- system/configuration.nix
+++ system/configuration.nix
@@ -35,6 +35,15 @@
     };
   };
 
+  environment.etc."wayland-sessions/mango.desktop".text = ''
+    [Desktop Entry]
+    Name=Mango
+    Comment=Start Mangowc Wayland Compositor
+    Exec=mango
+    Type=Application
+    DesktopNames=Mango
+  '';
+
   # Enable the KDE Plasma Desktop Environment.
   # services.desktopManager.plasma6.enable = true;
 
--- system/software/display/sddm.nix
+++ system/software/display/sddm.nix
@@ -1,43 +1,35 @@
 { config, pkgs, lib, defaultWallpaper, ... }: {
   # Set the sddm theme
 
-  environment.etc."wayland-sessions/mango.desktop".text = ''
-    [Desktop Entry]
-    Name=Mango
-    Comment=Start Mangowc Wayland Compositor
-    Exec=mango
-    Type=Application
-    DesktopNames=Mango
-  '';
-
   services.xserver.enable = true;
-  # services.xserver.displayManager.sddm.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
   # services.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.gnome.enable = false;
-  services.xserver.desktopManager.plasma5.enable = false;
+  # services.xserver.desktopManager.gnome.enable = true;
+  # services.xserver.desktopManager.plasma6.enable = true;
 
   services.displayManager.sddm = {
     enable = true; # Enable SDDM.
     wayland.enable = true;
     enableHidpi = true;
-    sugarCandyNix = {
-      enable = true;
-      settings = {
-        # General settings
-        Background = lib.cleanSource defaultWallpaper;
-        ScreenWidth = 2560;
-        ScreenHeight = 1440;
-        Font = "Iosevka nerd font";
-        # Form settings
-        HeaderText = "Welcome!";
-        FormPosition = "left";
-        HaveFormBackground = true;
-        PartialBlur = true;
-        # Customize colors
-        BackgroundColor = "#0c0b11";
-        MainColor = "#908caa";
-        AccentColor = "#9ccfd8";
-      };
-    };
+    theme = "default";
+    # sugarCandyNix = {
+    #   enable = true;
+    #   settings = {
+    #     # General settings
+    #     Background = lib.cleanSource defaultWallpaper;
+    #     ScreenWidth = 2560;
+    #     ScreenHeight = 1440;
+    #     Font = "Iosevka nerd font";
+    #     # Form settings
+    #     HeaderText = "Welcome!";
+    #     FormPosition = "left";
+    #     HaveFormBackground = true;
+    #     PartialBlur = true;
+    #     # Customize colors
+    #     BackgroundColor = "#0c0b11";
+    #     MainColor = "#908caa";
+    #     AccentColor = "#9ccfd8";
+    #   };
+    # };
   };
 }
